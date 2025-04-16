173a6bc45ac26f2705089c6faf96517ef56fb681
Kauanny19 <kauannycristina01n@gmail.com>
2025-04-16 16:18:00 -0300

    Lista Eventos

---
+++ src/pages/listEventos.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from "react";
+// Imports para criação de tabela
+import Table from "@mui/material/Table";
+import TableContainer from "@mui/material/TableContainer";
+// TableHead é onde colocamos os titulos
+import TableHead from "@mui/material/TableHead";
+// TableBody é onde colocamos o conteúdo
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import Paper from "@mui/material/Paper";
+import api from "../axios/axios";
+import { Button, IconButton, Alert, Snackbar } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+
+function listEventos() {
+  const [eventos, setEventos] = useState([]);
+  const navigate = useNavigate();
+
+  async function getEventos() {
+    // Chamada da Api
+    await api.getEventos().then(
+      (response) => {
+        console.log(response.data.events);
+        setEventos(response.data.events);
+      },
+      (error) => {
+        console.log("Erro ", error);
+      }
+    );
+  }
+
+  const listEventos = events.map((evento) => {
+    return (
+      <TableRow key={evento.id_evento}>
+        <TableCell align="center">{evento.nome}</TableCell>
+        <TableCell align="center">{evento.descrição}</TableCell>
+        <TableCell align="center">{evento.data_hora}</TableCell>
+        <TableCell align="center">{evento.local}</TableCell>      
+      </TableRow>
+    );
+  });
+
+  function logout() {
+    localStorage.removeItem("authenticated");
+    navigate("/");
+  }
+
+  useEffect(() => {
+    // if(!localStorage.getItem("authenticated")){
+    //   navigate("/");
+    // }
+    getEventos();
+  }, []);
+
+  return (
+    <div>
+      {events.length === 0 ? (
+        <h1>Carregando usuários</h1>
+      ) : (
+        <div>
+          <h5>Lista de eventos</h5>
+          <TableContainer component={Paper} style={{ margin: "2px" }}>
+            <Table size="small">
+              <TableHead
+                style={{ backgroundColor: "plum", borderStyle: "solid" }}
+              >
+                <TableRow>
+                  <TableCell align="center">Nome</TableCell>
+                  <TableCell align="center">Descrição</TableCell>
+                  <TableCell align="center">Data e Hora</TableCell>
+                  <TableCell align="center">Local</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>{listEventos}</TableBody>
+            </Table>
+          </TableContainer>
+          <Button fullWidth variant="contained" onClick={logout}>
+            SAIR
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+}
+export default listEventos;
