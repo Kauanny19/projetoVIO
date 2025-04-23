a91dd86405496c2e50aee28dd3b080f244ab0ef7
Kauanny19 <kauannycristina01n@gmail.com>
2025-04-23 08:10:52 -0300

    Lista de Eventos

--- src/pages/listEventos.jsx
+++ src/pages/listEventos.jsx
@@ -11,10 +11,32 @@ import TableCell from "@mui/material/TableCell";
 import Paper from "@mui/material/Paper";
 import api from "../axios/axios";
 import { Button, IconButton, Alert, Snackbar } from "@mui/material";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import { Link, useNavigate } from "react-router-dom";
 
 function listEventos() {
   const [events, setEventos] = useState([]);
+  const [alert, setAlert] = useState({
+    // Visibilidade (false = oculto; true: visível)
+    open: false,
+
+    // Nível do alerta (sucess, error, warning, etc)
+    severity:"",
+
+    // Mensagem que será exibida
+    message:""
+  });
+
+  //Função para exibir o alerta
+  const showAlert = (severity, message) => {
+    setAlert({open: true, severity, message})
+  };
+
+  //Fechar o alerta
+  const handleCloseAlert = () => {
+    setAlert({...alert, open: false})
+  };
+  
   const navigate = useNavigate();
 
   async function getEventos() {
@@ -30,14 +52,31 @@ function listEventos() {
     );
   }
 
+  async function deleteEvento(id) {
+    try{
+      await api.deleteEvento(id);
+      await getEventos();
+      showAlert("success", "Evento excluído com sucesso!");
+    }catch(error){
+      console.log("Erro ao deletar evento...", error);
+      showAlert("error", error.response.data.error);
+    }    
+  }
+
   const listEventos = events.map((evento) => {
     return (
       <TableRow key={evento.id_evento}>
         <TableCell align="center">{evento.nome}</TableCell>
         <TableCell align="center">{evento.descrição}</TableCell>
         <TableCell align="center">{evento.data_hora}</TableCell>
-        <TableCell align="center">{evento.local}</TableCell>      
+        <TableCell align="center">{evento.local}</TableCell>
+        <TableCell align="center">
+          <IconButton onClick={() => deleteEvento(evento.id)}>
+            <DeleteOutlineIcon color="error"/>
+          </IconButton>
+        </TableCell>      
       </TableRow>
+      
     );
   });
 
@@ -55,6 +94,14 @@ function listEventos() {
 
   return (
     <div>
+      <Snackbar open={alert.open} autoHideDuration={3000} onClose={handleCloseAlert} 
+      anchorOrigin={{vertical:"top", horizontal:"center"}}>
+        <Alert onClose={handleCloseAlert}
+        severity={alert.severity}
+        sx={{width: "100%"}}>
+          {alert.message}
+        </Alert>
+      </Snackbar>
       {events.length === 0 ? (
         <h1>Carregando eventos</h1>
       ) : (
@@ -70,6 +117,8 @@ function listEventos() {
                   <TableCell align="center">Descrição</TableCell>
                   <TableCell align="center">Data e Hora</TableCell>
                   <TableCell align="center">Local</TableCell>
+                  <TableCell align="center">Ações</TableCell>
+
                 </TableRow>
               </TableHead>
               <TableBody>{listEventos}</TableBody>
