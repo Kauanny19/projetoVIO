26ce8446caa4563ada8006e7b0ea61889954e863
Kauanny19 <kauannycristina01n@gmail.com>
2025-08-18 12:54:56 -0300

    Imagem na Interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import ListUsers from "./pages/listUsers";
+import ListEventos from "./pages/listEventos";
 import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
-import ListEventos from "./pages/listEventos";
 import ProtectedRoute from "./components/ProtectedRoute";
 import CreateEvent from "./pages/CreateEvent";
 
@@ -13,21 +15,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/eventos" element={
-            <ProtectedRoute>
-              <ListEventos/>
-            </ProtectedRoute>
-          }/>
-          <Route path="/users" element={
-            <ProtectedRoute>
-              <ListUsers />
-            </ProtectedRoute>
-          }/>
-          <Route path="/CreateEvent" element={
-            <ProtectedRoute>
-              <CreateEvent />
-            </ProtectedRoute>
-          }/>
+          <Route path="/users" element={<ProtectedRoute><ListUsers/></ProtectedRoute>}/>
+          <Route path="/events" element={<ProtectedRoute><ListEventos/></ProtectedRoute>}/>
+          <Route path="/CreateEvent" element={<ProtectedRoute><CreateEvent/></ProtectedRoute>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/listEventos.jsx
+++ src/pages/listEventos.jsx
@@ -1,166 +1,187 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 // Imports para criação de tabela
-import Table from "@mui/material/Table";
-import TableContainer from "@mui/material/TableContainer";
+import Table from '@mui/material/Table';
+import TableContainer from '@mui/material/TableContainer';
 // TableHead é onde colocamos os titulos
-import TableHead from "@mui/material/TableHead";
+import TableHead from '@mui/material/TableHead';
 // TableBody é onde colocamos o conteúdo
-import TableBody from "@mui/material/TableBody";
-import TableRow from "@mui/material/TableRow";
-import TableCell from "@mui/material/TableCell";
-import Paper from "@mui/material/Paper";
-import api from "../axios/axios";
-import { Button, IconButton, Alert, Snackbar } from "@mui/material";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-import { Link, useNavigate } from "react-router-dom";
-import ModalCriarIngresso from "../components/ModalCriarIngresso";
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Paper from '@mui/material/Paper';
+import api from '../axios/axios'
+import { Button, IconButton, Alert, Snackbar } from '@mui/material';
+import { Link , useNavigate } from 'react-router-dom';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ModalCriarIngresso from '../components/ModalCriarIngresso';
 
 function listEventos() {
-  const [events, setEventos] = useState([]);
+  const [events,setEventos] = useState([]);
   const [alert, setAlert] = useState({
-    // Visibilidade (false = oculto; true: visível)
+    //Visibilidade (false = oculta; true = visível )
     open: false,
 
-    // Nível do alerta (sucess, error, warning, etc)
-    severity: "",
+    //Nível do alerta (sucess, error, warning, etc)
+    severity:"",
 
-    // Mensagem que será exibida
-    message: "",
+    //Mensagem que será exibida
+    message:""
   });
 
-  //Função para exibir o alerta
+  //Função para exibir o alerta 
   const showAlert = (severity, message) => {
-    setAlert({ open: true, severity, message });
+    setAlert({open: true, severity, message})
   };
 
-  //Fechar o alerta
+  //fechar o alerta
   const handleCloseAlert = () => {
-    setAlert({ ...alert, open: false });
+    setAlert({...alert, open:false})
   };
 
   const navigate = useNavigate();
 
-  async function getEventos() {
+  async function getEventos(){
     // Chamada da Api
     await api.getEventos().then(
-      (response) => {
-        console.log(response.data.events);
-        setEventos(response.data.events);
-      },
-      (error) => {
-        console.log("Erro ", error);
+      (response)=>{
+        console.log(response.data.events)
+        setEventos(response.data.events)
+      },(error)=>{
+        console.log("Erro ",error)
       }
-    );
+    )
   }
 
-  async function deleteEvento(id) {
-    try {
-      await api.deleteEvento(id);
+  async function deleteEventos(id){
+    try{
+      await api.deleteEventos(id);
       await getEventos();
-      showAlert("success", "Evento excluído com sucesso!");
-    } catch (error) {
+      showAlert("success", "Evento excluído com sucesso!")
+    }catch(error){
       console.log("Erro ao deletar evento...", error);
       showAlert("error", error.response.data.error);
+
     }
   }
 
-  const listEventos = events.map((evento) => {
-    return (
+  const listEventos = events.map((evento)=>{
+    return(
       <TableRow key={evento.id_evento}>
         <TableCell align="center">{evento.nome}</TableCell>
-        <TableCell align="center">{evento.descrição}</TableCell>
+        <TableCell align="center">{evento.descricao}</TableCell>
         <TableCell align="center">{evento.data_hora}</TableCell>
         <TableCell align="center">{evento.local}</TableCell>
+        <TableCell align="center">{evento.fk_id_organizador}</TableCell>
+        <TableCell>
+          <img 
+          src={`http://localhost:5000/api/v1/evento/imagem/${evento.id_evento}`}
+          alt="Imagem do evento"
+          style={{width:"80px", height:"80px", objectFit:"cover"}}
+          />
+        </TableCell>
+
         <TableCell align="center">
-          <IconButton onClick={() => deleteEvento(evento.id)}>
-            <DeleteOutlineIcon color="error" />
+          <IconButton onClick={() => deleteEventos(evento.id)}>
+            <DeleteOutlineIcon color="error"/>
           </IconButton>
         </TableCell>
         <TableCell align="center">
           <IconButton onClick={() => abrirModalIngresso(evento)}>
-            Adicionar
+            Adicionar 
           </IconButton>
         </TableCell>
+
+
       </TableRow>
-    );
+    )
   });
 
-  function logout() {
+  function logout(){
     localStorage.removeItem("authenticated");
-    navigate("/");
+    navigate("/")
   }
 
-  useEffect(() => {
-    // if(!localStorage.getItem("authenticated")){
-    //   navigate("/");
-    // }
-    getEventos();
-  }, []);
+  useEffect(()=>{getEventos();},[]);
 
-  const [eventoSelecionado, setEventoSelecionado] = useState("");
-  const [modalOpen, setModalOpen] = useState(false);
+const [eventoSelecionado, setEventoSelecionado] = useState("");
+const [modalOpen, setModalOpen] = useState(false);
 
-  const abrirModalIngresso = (evento) => {
-    setEventoSelecionado(evento);
-    setModalOpen(true);
-  };
+const abrirModalIngresso = (evento) => {
+  setEventoSelecionado(evento);
+  setModalOpen(true);
+};
 
-  const fecharModalIngresso = () => {
-    setModalOpen(false);
-    setEventoSelecionado("");
-  };
+const fecharModalIngresso = () => {
+  setModalOpen(false);
+  setEventoSelecionado("");
+};
+
 
   return (
     <div>
-      <Snackbar
-        open={alert.open}
-        autoHideDuration={3000}
-        onClose={handleCloseAlert}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-      >
-        <Alert
-          onClose={handleCloseAlert}
-          severity={alert.severity}
-          sx={{ width: "100%" }}
-        >
+      <Snackbar open={alert.open} autoHideDuration={3000} onClose={handleCloseAlert}
+      anchorOrigin={{vertical:"top", horizontal:"center"}}>
+        <Alert onClose={handleCloseAlert}
+        severity={alert.severity} 
+        sx={{width:"100%"}}>
           {alert.message}
         </Alert>
+
       </Snackbar>
 
       <ModalCriarIngresso
-        open={modalOpen}
-        onClose={fecharModalIngresso}
-        eventoSelecionado={eventoSelecionado}
-      />
+  open={modalOpen}
+  onClose={fecharModalIngresso}
+  eventoSelecionado={eventoSelecionado}
+/>
 
-      {events.length === 0 ? (
-        <h1>Carregando eventos</h1>
-      ) : (
-        <div>
-          <h5>Lista de eventos</h5>
-          <TableContainer component={Paper} style={{ margin: "2px" }}>
-            <Table size="small">
-              <TableHead
-                style={{ backgroundColor: "plum", borderStyle: "solid" }}
-              >
-                <TableRow>
-                  <TableCell align="center">Nome</TableCell>
-                  <TableCell align="center">Descrição</TableCell>
-                  <TableCell align="center">Data e Hora</TableCell>
-                  <TableCell align="center">Local</TableCell>
-                  <TableCell align="center">Excluir</TableCell>
-                  <TableCell align="center">Criar Ingresso</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>{listEventos}</TableBody>
-            </Table>
-          </TableContainer>
-          <Button fullWidth variant="contained" onClick={logout}>
-            SAIR
-          </Button>
-        </div>
+      {events.length === 0 ?(<h1>Carregando eventos</h1>): (
+      <div>
+        <h5>Lista de Eventos</h5>
+        <TableContainer component={Paper} style={{margin:"2px"}}>
+          <Table size="small">
+            <TableHead style={{backgroundColor: "lightskyblue", borderStyle:"solid"}}>
+              <TableRow>
+                <TableCell align="center">
+                  Nome
+                </TableCell>
+                <TableCell align="center">
+                  Descrição
+                </TableCell>
+                <TableCell align="center">
+                  Data hora
+                </TableCell>
+                <TableCell align="center">
+                  Local
+                </TableCell>
+                <TableCell align="center">
+                  Id Organizador
+                </TableCell>
+                <TableCell align="center">
+                  Imagem
+                </TableCell>
+                <TableCell align="center">
+                  Excluir
+                </TableCell>
+                <TableCell align="center">
+                  Criar ingresso
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{listEventos}</TableBody>
+          </Table>
+        </TableContainer>
+      <Button
+      fullWidth
+      variant='contained'
+      onClick={logout}
+      >
+        SAIR
+      </ Button>
+      </div>
       )}
     </div>
-  );
+  )
 }
-export default listEventos;
+export default listEventos
